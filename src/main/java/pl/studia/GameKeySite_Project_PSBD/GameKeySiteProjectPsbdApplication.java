eb83de9bb85af8444e936ce62977fdb838c79f4e
AdrianJurak <adrianjurak@studenci.collegiumwitelona.pl>
2025-05-20 22:44:27 +0200

    init project

---
+++ src/main/java/pl/studia/GameKeySite_Project_PSBD/GameKeySiteProjectPsbdApplication.java
@@ -0,0 +1,13 @@
+package pl.studia.GameKeySite_Project_PSBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameKeySiteProjectPsbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameKeySiteProjectPsbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/studia/GameKeySite_Project_PSBD/GameKeySiteProjectPsbdApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.studia.GameKeySite_Project_PSBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameKeySiteProjectPsbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
